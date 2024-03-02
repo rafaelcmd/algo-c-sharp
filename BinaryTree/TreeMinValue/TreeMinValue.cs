35b07eee83f83cca4431fca2153a650326023fae
Rafael Costa <rafaelcmd@gmail.com>
2024-03-02 00:38:52 -0300

    added comments

--- BinaryTree/TreeMinValue/TreeMinValue.cs
+++ BinaryTree/TreeMinValue/TreeMinValue.cs
@@ -4,6 +4,26 @@ namespace BinaryTree.TreeMinValue;
 
 public static class TreeMinValue
 {
+    /// <summary>
+    /// Recursively finds the minimum value in the tree starting from the specified root node.
+    /// </summary>
+    /// <param name="root">The root node of the tree to find the minimum value for.</param>
+    /// <returns>The minimum value in the tree.</returns>
+    /// <remarks>
+    /// Advantages of using recursion:
+    /// 1. **Simplicity**: Recursive algorithms are often simpler to implement and understand than their iterative counterparts.
+    /// 2. **Natural Fit**: Recursion mirrors the hierarchical structure of trees, making it intuitive for tree traversal and aggregation operations.
+    /// 3. **Readability**: Recursive algorithms can be more readable and expressive, resembling pseudocode or natural language descriptions of the problem-solving process.
+    /// 4. **Ease of Implementation**: Recursive algorithms closely follow the natural definition of the problem, making them easier to implement and understand.
+    ///
+    /// Time Complexity: O(n)
+    /// The time complexity of this method is O(n), where n is the number of nodes in the tree. This is because the algorithm visits each node of the tree once to find the minimum value.
+    ///
+    /// Space Complexity: O(h)
+    /// The space complexity of this method is O(h), where h is the height of the tree. This is because the recursive calls consume space on the call stack, proportional to the height of the tree.
+    /// In a balanced tree, where the height is logarithmic in the number of nodes (h = log n), the space complexity becomes O(log n).
+    /// However, in the worst case of an unbalanced tree (h = n), the space complexity becomes O(n).
+    /// </remarks>
     public static int MinValueWithRecursion(Node? root)
     {
         if (root == null)
@@ -15,6 +35,27 @@ public static class TreeMinValue
         return Math.Min(root.Value, Math.Min(leftMin, rightMin));
     }
     
+    /// <summary>
+    /// Iteratively finds the minimum value in the tree starting from the specified root node, and using a Queue.
+    /// </summary>
+    /// <param name="root">The root node of the tree to find the minimum value for.</param>
+    /// <returns>The minimum value in the tree.</returns>
+    /// <remarks>
+    /// Advantages of using iteration and a queue:
+    /// 1. **Space Efficiency**: Iterative algorithms often consume less space than their recursive counterparts, especially for large trees.
+    /// This is because iterative algorithms do not rely on the call stack to manage recursive calls, which can lead to stack overflow errors for very deep trees.
+    /// 2. **Tail Recursion Optimization**: Some programming languages and compilers do not optimize tail-recursive calls,
+    /// which can lead to stack overflow errors for very deep trees. Iterative algorithms avoid this issue by using an explicit stack or queue to manage traversal.
+    ///
+    /// Time Complexity: O(n)
+    /// The time complexity of this method is O(n), where n is the number of nodes in the tree. This is because the algorithm visits each node of the tree once to find the minimum value.
+    ///
+    /// Space Complexity: O(h)
+    /// The space complexity of this method is O(h), where h is the height of the tree. This is because the algorithm uses a queue to store nodes during traversal,
+    /// and the queue can potentially contain all nodes of the tree in the worst case.
+    /// In a balanced tree, where the height is logarithmic in the number of nodes (h = log n), the space complexity becomes O(log n).
+    /// However, in the worst case of an unbalanced tree (h = n), the space complexity becomes O(n).
+    /// </remarks>
     public static int MinValueWithIterationAndQueue(Node? root)
     {
         if (root == null)
@@ -41,6 +82,27 @@ public static class TreeMinValue
         return minValue;
     }
     
+    /// <summary>
+    /// Iteratively finds the minimum value in the tree starting from the specified root node, and using a Stack.
+    /// </summary>
+    /// <param name="root">The root node of the tree to find the minimum value for.</param>
+    /// <returns>The minimum value in the tree.</returns>
+    /// <remarks>
+    /// Advantages of using iteration and a stack:
+    /// 1. **Space Efficiency**: Iterative algorithms often consume less space than their recursive counterparts, especially for large trees.
+    /// This is because iterative algorithms do not rely on the call stack to manage recursive calls, which can lead to stack overflow errors for very deep trees.
+    /// 2. **Tail Recursion Optimization**: Some programming languages and compilers do not optimize tail-recursive calls,
+    /// which can lead to stack overflow errors for very deep trees. Iterative algorithms avoid this issue by using an explicit stack or queue to manage traversal.
+    ///
+    /// Time Complexity: O(n)
+    /// The time complexity of this method is O(n), where n is the number of nodes in the tree. This is because the algorithm visits each node of the tree once to find the minimum value.
+    ///
+    /// Space Complexity: O(h)
+    /// The space complexity of this method is O(h), where h is the height of the tree. This is because the algorithm uses a stack to store nodes during traversal,
+    /// and the stack can potentially contain all nodes of the tree in the worst case.
+    /// In a balanced tree, where the height is logarithmic in the number of nodes (h = log n), the space complexity becomes O(log n).
+    /// However, in the worst case of an unbalanced tree (h = n), the space complexity becomes O(n).
+    /// </remarks>
     public static int MinValueWithIterationAndStack(Node? root)
     {
         if (root == null)
